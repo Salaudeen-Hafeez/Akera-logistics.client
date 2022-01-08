241961551904c992c16e2bd2f88f63b7d58b9e19
Salaudeen Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-01-08 15:13:38 +0100

    router implementation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 // const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
@@ -16,8 +15,6 @@ module.exports = {
       },
     },
   },
-  variants: {
-    extend: {},
-  },
+
   plugins: [],
 };
